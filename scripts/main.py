7e2ff2402759282c64251da0eb7f6e18925a79a9
Dylan Snow <dyl.j.snow@gmail.com>
2023-09-15 22:32:50 -0700

    WIP: Input for divisions, max and min

--- scripts/main.py
+++ scripts/main.py
@@ -22,9 +22,17 @@ def get_stepper_motor_serial_numbers():
 def get_axes():
     return ["X", "Y", "Z"]
 
+def get_target_position_buttons():
+    """Return the buttons that can be used to change the target position"""
+    return ["plus_10_stepper", "plus_100_stepper", "plus_1000_stepper", "minus_10_stepper", "minus_100_stepper", "minus_1000_stepper"]
+
 def get_buttons_to_initialize():
     """Return a list of buttons that should be disabled until the stepper motor is initialized"""
-    return ["energize_stepper", "deenergize_stepper", "zero_stepper", "plus_10_stepper", "plus_100_stepper", "minus_10_stepper", "minus_100_stepper"]
+    return ["energize_stepper", "deenergize_stepper", "zero_stepper"] + get_target_position_buttons()
+
+def get_current_limits():
+    """Return a list of current limits"""
+    pass
 
 class CurrentPositionDisplay(Static):
     """A widget to display the current position"""
@@ -32,11 +40,11 @@ class CurrentPositionDisplay(Static):
 
     def on_mount(self) -> None:
         """Event handler called when widget is added to the app"""
-        pass
+        self.update("Current position: \n" + str(self.current_position))
 
     def watch_current_position(self) -> None:
         """Automatically (via Textual) update listen for changes to current_position"""
-        self.update("Current position: " + str(self.current_position))
+        self.update("Current position: \n" + str(self.current_position))
 
     def update_current_position(self, new_position: float) -> None:
         self.current_position = new_position
@@ -47,31 +55,75 @@ class TargetPositionDisplay(Static):
 
     def on_mount(self) -> None:
         """Event handler called when widget is added to the app"""
-        pass
+        self.update("Target position: \n" + str(self.target_position))
 
     def watch_target_position(self) -> None:
         """Automatically (via Textual) update listen for changes to target_position"""
-        self.update("Target position: " + str(self.target_position))
+        self.update("Target position: \n" + str(self.target_position))
 
     def update_target_position(self, new_position: float) -> None:
         self.target_position = new_position
 
+class MaxPositionDisplay(Static):
+    """A widget to display the maximum value the stepper motor can move to"""
+    max_position = reactive(0)
+
+    def on_mount(self) -> None:
+        """Event handler called when widget is added to the app"""
+        self.update("Maximum position: \n" + str(self.max_position))
+
+    def watch_max_position(self) -> None:
+        """Automatically (via Textual) update listen for changes to max_position"""
+        self.update("Maximum position: \n" + str(self.max_position))
+
+    def update_max_position(self, new_position: float) -> None:
+        self.max_position = new_position
+
+class MinPositionDisplay(Static):
+    """A widget to display the minimum value the stepper motor can move to"""
+    min_position = reactive(0)
+
+    def on_mount(self) -> None:
+        """Event handler called when widget is added to the app"""
+        self.update("Minimum position: \n" + str(self.min_position))
+
+    def watch_min_position(self) -> None:
+        """Automatically (via Textual) update listen for changes to min_position"""
+        self.update("Minimum position: \n" + str(self.min_position))
+
+    def update_min_position(self, new_position: float) -> None:
+        self.min_position = new_position
+
+class CurrentLimitDisplay(Static): 
+    """A widget to display the current limit of the stepper motor"""
+    current_limit = reactive(0)
+
+    def on_mount(self) -> None:
+        """Event handler called when widget is added to the app"""
+        self.update("Current limit: \n" + str(self.current_limit))
+
+    def watch_current_limit(self) -> None:
+        """Automatically (via Textual) update listen for changes to current_limit"""
+        self.update("Current limit: \n" + str(self.current_limit))
+
+    def update_current_limit(self, new_limit: float) -> None:
+        self.current_limit = new_limit
+
 class StepperMotor(Static):
     """A stepper motor interface"""
-
     axes = get_axes()
     axis = reactive("")
     serial_numbers = get_stepper_motor_serial_numbers()
     serial_number = reactive("")
     current_position = reactive(0)
     target_position = reactive(0)
-    max_value = reactive(0)
-    min_value = reactive(0)
-    home_position = reactive(0)
+    max_position = reactive(0)
+    min_position = reactive(0)
     moving = reactive(False)
     energized = reactive(False)
     initialized = reactive(False)
-    tic = None
+    divisions = reactive(0) # How many divisions (between maximum and minimum position) to take photos
+    tic = None # USB connection to the stepper motor
 
     def reset(self):
         self.axis = ""
@@ -80,9 +132,10 @@ class StepperMotor(Static):
         self.query_one(CurrentPositionDisplay).current_position = self.current_position
         self.target_position = 0
         self.query_one(TargetPositionDisplay).current_position = self.target_position
-        self.max_value = 0
-        self.min_value = 0
-        self.home_position = 0
+        self.max_position = 0
+        self.query_one(MaxPositionDisplay).max_position = self.max_position
+        self.min_position = 0
+        self.query_one(MinPositionDisplay).min_position = self.min_position
         self.moving = False
 
     def on_mount(self) -> None:
@@ -106,7 +159,12 @@ class StepperMotor(Static):
         if self.tic and self.energized:
             self.tic.set_target_position(self.target_position)
             self.query_one(TargetPositionDisplay).target_position = self.target_position
-
+            if self.target_position > self.max_position:
+                self.max_position = self.target_position
+                self.query_one(MaxPositionDisplay).max_position = self.max_position
+            if self.target_position < self.min_position:
+                self.min_position = self.target_position
+                self.query_one(MinPositionDisplay).min_position = self.min_position
 
     def zero_stepper(self):
         if self.tic and self.energized:
@@ -116,9 +174,6 @@ class StepperMotor(Static):
             self.query_one(CurrentPositionDisplay).current_position = self.current_position
             self.target_position = 0
             self.query_one(TargetPositionDisplay).target_position = self.target_position
-            self.max_value = 0
-            self.min_value = 0
-            self.home_position = 0
             self.moving = False
 
     def update_initialized(self, event: Button.Pressed) -> None:
@@ -127,7 +182,7 @@ class StepperMotor(Static):
             if button.id in ["energize_stepper", "deenergize_stepper", "zero_stepper"]:
                 button.disabled = not self.initialized
         for input in self.query(Input):
-            if input.id in ["target_position_stepper", "max_value_stepper", "min_value_stepper"]:
+            if input.id in ["target_position_stepper", "max_position_stepper", "min_position_stepper"]:
                 input.disabled = not self.initialized
         if self.initialized:
             self.add_class("initialized")
@@ -138,7 +193,7 @@ class StepperMotor(Static):
             self.remove_class("deenergized")
             self.energized = False
         for select in self.query(Select):
-            if select.id in ["axis_stepper", "serial_stepper", "zero_stepper"]:
+            if select.id in ["axis_stepper", "serial_stepper", "zero_stepper", "divisions_stepper"]:
                 select.disabled = self.initialized
         for button in self.query(Button):
             if button.id in get_buttons_to_initialize():
@@ -176,10 +231,14 @@ class StepperMotor(Static):
             self.target_position += 10
         elif button_id == "plus_100_stepper":
             self.target_position += 100
+        elif button_id == "plus_1000_stepper":
+            self.target_position += 1000
         elif button_id == "minus_10_stepper":
             self.target_position -= 10
         elif button_id == "minus_100_stepper":
             self.target_position -= 100
+        elif button_id == "minus_1000_stepper":
+            self.target_position -= 1000
         else:
             self.target_position = self.target_position
         print(self.id, " target position: ", self.target_position)
@@ -196,10 +255,15 @@ class StepperMotor(Static):
             self.update_deenergized(event)
         elif event.button.id == "zero_stepper":
             self.zero_stepper()
-        elif event.button.id in ["plus_10_stepper", "plus_100_stepper", "minus_10_stepper", "minus_100_stepper"]:
+        elif event.button.id in get_target_position_buttons():
             self.update_target_position(event)
             
-
+    @on(Input.Changed)
+    def on_input_changed(self, event: Input.Changed) -> None:
+        """Handle an input change"""
+        print(self.id, " input changed: ", event.value)
+        if event.input.id == "divisions_stepper":
+            self.divisions = event.value
 
     @on(Select.Changed)
     def on_select_changed(self, event: Select.Changed) -> None:
@@ -222,18 +286,20 @@ class StepperMotor(Static):
         yield Button("Energize", id="energize_stepper", variant="success", disabled=True)
         yield Button("Zero", id="zero_stepper", variant="primary", disabled=True)
         yield Button("Deenergize", id="deenergize_stepper", variant="error", disabled=True)
-        yield Label("Current position: ", id="current_position_label")
         yield CurrentPositionDisplay()
-        yield Label("Target position: ", id="target_position_label")
         yield TargetPositionDisplay()
-        yield Label("Max value: ", id="max_value_label")
-        yield Input(id="max_value_stepper", value=str(self.max_value), disabled=True)
-        yield Label("Min value: ", id="min_value_label")
-        yield Input(id="min_value_stepper", value=str(self.min_value), disabled=True)
-        yield Button("+10", id="plus_10_stepper", variant="primary", disabled=True)
+        yield MinPositionDisplay()
+        yield MaxPositionDisplay()
+        yield Button("+1000", id="plus_1000_stepper", variant="primary", disabled=True)
         yield Button("+100", id="plus_100_stepper", variant="primary", disabled=True)
+        yield Button("+10", id="plus_10_stepper", variant="primary", disabled=True)
         yield Button("-10", id="minus_10_stepper", variant="primary", disabled=True)
         yield Button("-100", id="minus_100_stepper", variant="primary", disabled=True)
+        yield Button("-1000", id="minus_1000_stepper", variant="primary", disabled=True)
+        yield Label("Divisions: ", id="photo_divisions_label")
+        yield Input(placeholder="Divisions", id="divisions_stepper", disabled=True)
+        yield Input(placeholder="Set max: ", id="max_position_stepper", disabled=True)
+        yield Input(placeholder="Set min: ", id="min_position_stepper",  disabled=True)
 
 
 class Scant(App):
